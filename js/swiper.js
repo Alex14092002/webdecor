369e3633942e937d51bb3a1d0ece70af7d36d9b0
Your Name <2051010343tuan@ou.edu.vn>
2023-10-10 11:34:57 +0700

    update

--- js/swiper.js
+++ js/swiper.js
@@ -86,3 +86,34 @@ Object.assign(swiperElss, {
     },
   },
 });
+
+
+
+const swiperElsss = document.querySelector(".mySwipersss");
+
+swiperElsss.initialize();
+
+Object.assign(swiperElsss, {
+  
+  slidesPerView: 3, 
+ 
+  breakpoints: {
+    "@0.00": {
+      slidesPerView: 3,
+      spaceBetween: 10,
+    },
+    "@0.75": {
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    "@1.00": {
+      slidesPerView: 4,
+      spaceBetween: 40,
+    },
+    "@1.50": {
+      slidesPerView: 8,
+      spaceBetween: 50,
+    },
+  },
+});
+
